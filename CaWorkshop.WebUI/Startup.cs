003a27023a600b83a062632100ac4d61427e0b82
Kaha Mason www.ssw.com.au <KahaMason@ssw.com.au>
2020-02-25 11:21:16 +1000

    Getting started done

--- CaWorkshop.WebUI/Startup.cs
+++ CaWorkshop.WebUI/Startup.cs
@@ -98,7 +98,8 @@ namespace CaWorkshop.WebUI
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    // spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
